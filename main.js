62fb367b5a6a73188ba2a06c5464a290a3d4a3c4
jbmeiners <jbmeiners@gmail.com>
2025-07-13 17:22:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-const countryAlbumLinks = {
-  "ECU": "https://photos.app.goo.gl/2WRE3e5T3aumguWS9",
-  "CHN": "https://photos.app.goo.gl/N9SYsuYxfsLpmN35A"
-};
-
-const stateAlbumLinks = {
-  "SC": "https://photos.app.goo.gl/juKhxjzKhdjEWcGG6"
-};
-
-const map = L.map('map').setView([20, 0], 2);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
-
-let countriesLayer;
-let usStatesLayer;
-
-// Load countries GeoJSON
 fetch('countries.geojson')
   .then(res => res.json())
   .then(data => {
@@ -42,45 +23,6 @@ fetch('countries.geojson')
           layer.on('click', () => layer.openPopup());
         }
       }
-    }).addTo(map);
-  });
-
-// Load US States GeoJSON
-fetch('us-states.geojson')
-  .then(res => res.json())
-  .then(data => {
-    usStatesLayer = L.geoJSON(data, {
-      style: feature => ({
-        fillColor: stateAlbumLinks[feature.properties.postal] ? "#ff8800" : "#00aaff",
-        fillOpacity: stateAlbumLinks[feature.properties.postal] ? 0.6 : 0.2,
-        weight: 1,
-        color: "#666"
-      }),
-      onEachFeature: (feature, layer) => {
-        const code = feature.properties.postal;
-        const name = feature.properties.name;
-        layer.bindTooltip(name);
-
-        if (stateAlbumLinks[code]) {
-          layer.bindPopup(`
-            <strong>${name}</strong><br>
-            <a href="${stateAlbumLinks[code]}" target="_blank" rel="noopener noreferrer">
-              📸 View pictures from ${name}
-            </a>
-          `);
-          layer.on('click', () => layer.openPopup());
-        }
-      }
     });
+    countriesLayer.addTo(map);  // Add countries layer on load
   });
-
-// Control visibility based on zoom level
-map.on('zoomend', () => {
-  if (map.getZoom() >= 5) {
-    if (countriesLayer) map.removeLayer(countriesLayer);
-    if (usStatesLayer) usStatesLayer.addTo(map);
-  } else {
-    if (usStatesLayer) map.removeLayer(usStatesLayer);
-    if (countriesLayer) countriesLayer.addTo(map);
-  }
-});
