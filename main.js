0ada79a2bbf10e9e9322063e373b4b07d41395fd
jbmeiners <jbmeiners@gmail.com>
2025-07-13 17:32:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -28,4 +28,62 @@ function styleCountries(feature) {
 function styleStates(feature) {
   return {
     fillColor: stateAlbumLinks[feature.properties.postal] ? "#ff8800" : "#00aaff",
-    fillOpacity:
+    fillOpacity: stateAlbumLinks[feature.properties.postal] ? 0.6 : 0.2,
+    weight: 1,
+    color: "#666"
+  };
+}
+
+function onEachCountry(feature, layer) {
+  const code = feature.id;
+  const name = feature.properties.name;
+  layer.bindTooltip(name);
+  if (countryAlbumLinks[code]) {
+    const popupHtml = `
+      <strong>${name}</strong><br>
+      <a href="${countryAlbumLinks[code]}" target="_blank" rel="noopener noreferrer">
+        📸 View pictures from ${name}
+      </a>`;
+    layer.bindPopup(popupHtml);
+    layer.on('click', () => layer.openPopup());
+  }
+}
+
+function onEachState(feature, layer) {
+  const code = feature.properties.postal;
+  const name = feature.properties.name;
+  layer.bindTooltip(name);
+  if (stateAlbumLinks[code]) {
+    const popupHtml = `
+      <strong>${name}</strong><br>
+      <a href="${stateAlbumLinks[code]}" target="_blank" rel="noopener noreferrer">
+        📸 View pictures from ${name}
+      </a>`;
+    layer.bindPopup(popupHtml);
+    layer.on('click', () => layer.openPopup());
+  }
+}
+
+fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
+  .then(res => res.json())
+  .then(data => {
+    countriesLayer = L.geoJSON(data, {
+      style: styleCountries,
+      onEachFeature: onEachCountry
+    });
+    countriesLayer.addTo(map);
+    console.log("Countries layer loaded");
+  })
+  .catch(err => console.error("Countries GeoJSON load error:", err));
+
+fetch('https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_500k.json')
+  .then(res => res.json())
+  .then(data => {
+    usStatesLayer = L.geoJSON(data, {
+      style: styleStates,
+      onEachFeature: onEachState
+    });
+    usStatesLayer.addTo(map);
+    console.log("US States layer loaded");
+  })
+  .catch(err => console.error("US States GeoJSON load error:", err));
